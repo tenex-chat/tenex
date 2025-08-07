e94cfe505cd910a7990a0776444e1f183e6f4479
pablof7z <pfer@me.com>
2025-08-07 11:31:50 +0300

    refactor: Remove unused files

--- src/browser.ts
+++
@@ -1,5 +0,0 @@
-// Browser-compatible exports for TENEX CLI package
-// Only includes exports that work in browser environments
-export { NDKAgent } from "./events/NDKAgent";
-export { NDKAgentLesson } from "./events/NDKAgentLesson";
-export { NDKMCPTool } from "./events/NDKMCPTool";
